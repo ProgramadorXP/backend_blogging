f3b16dfb3c688a1d3c2342214f4cb01e251b0771
Jonhatan <jonyvilla44@gmail.com>
2024-12-21 17:46:57 -0600

    User route and controller settings.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,166 +1,10 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import { Op } from "sequelize";
 import User from "../models/user";
 
-//Get All Users
-export const getAllUsers = async (req: Request, res: Response) => {
-  //It's not necessary to implement middlewares for this route because it's doesn't have any req.body or req.params
-
-  //Logic to get all users
-  try {
-    const users = await User.findAll();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Error to get all users" });
-  }
-};
-
-//Get User By ID
-export const getUserById = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  // If there are errors, return them in the response
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-
-  //Logic to get user by id
-  const { id } = req.params;
-
-  try {
-    const user = await User.findByPk(id);
-    if (!user) {
-      res.status(404).json({ error: "User not found" });
-      return;
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Error to get user by id" });
-  }
-};
-
-// Create User
-export const createUser = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  //If there are errors, return them in the response
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-
-  //Logic to create user(register)
-  const { username, email, password, confirm_password, profile_image } =
-    req.body;
-
-  try {
-    const user = await User.create({
-      username,
-      email,
-      password,
-      confirm_password,
-      profile_image,
-    });
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Error to create user" });
-  }
-};
-
-//Update User
-export const updateUser = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-
-  try {
-    const { id } = req.params; //Id of the user to update
-    const { username, email, password, profile_image } = req.body; //Data to update
-
-    try {
-      //Search for the user id
-      const user = await User.findByPk(id);
-      if (!user) {
-        res.status(404).json({ error: "User not found" });
-        return;
-      }
-
-      //Check if the email is already in use
-      if (email && email !== user.email) {
-        const emailExists = await User.findOne({ where: { email } });
-        if (emailExists) {
-          res.status(400).json({ error: "The email already exists" });
-          return;
-        }
-      }
-
-      //Check if the username is already in use
-      if (username && username !== user.username) {
-        const usernameExists = await User.findOne({ where: { username } });
-        if (usernameExists) {
-          res.status(400).json({ error: "The username already exists" });
-          return;
-        }
-      }
-
-      //Hash the password if it is provided
-      if (password) {
-        user.password = await bcrypt.hash(password, 10);
-      }
-
-      //Update the fields allowed
-      if (email) user.email = email;
-      if (username) user.username = username;
-      if (profile_image) user.profile_image = profile_image;
-
-      await user.save(); // Save the changes
-      res.status(200).json({ message: "Data updated successfully", user });
-    } catch (error) {
-      res.status(500).json({ errors: "Error to update user" });
-    }
-  } catch (error) {
-    res.status(500).json({ errors: "Error to update user" });
-    return;
-  }
-};
-
-export const deleteUser = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-
-  try {
-    const { id } = req.params; //Id of the user to delete
-
-    try {
-      //Search for the user id
-      const user = await User.findByPk(id);
-      if (!user) {
-        res.status(404).json({ error: "User not found" });
-        return;
-      }
-
-      await user.destroy(); // Delete the user
-      res.status(200).json({ message: "User deleted successfully" });
-    } catch (error) {
-      res.status(500).json({ errors: "Error to delete user" });
-    }
-  } catch (error) {
-    res.status(500).json({ errors: "Error to delete user" });
-    return;
-  }
-};
-
+//Login User
 export const loginUser = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
@@ -202,6 +46,7 @@ export const loginUser = async (req: Request, res: Response) => {
   }
 };
 
+//Create Account
 export const createAccount = async (req: Request, res: Response) => {
   const { username, email, password, confirm_password } = req.body;
   
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,10 @@
 import { Router } from 'express';
-import { getAllUsers, getUserById, loginUser, updateUser, createAccount } from '../controllers/userController';
-import { register, validateGetUser, login, validateUpdateUser } from '../middlewares/AuthValidation';
-import { authenticateToken } from '../middlewares/auth';
+import { loginUser, createAccount } from '../controllers/userController';
+import { register, login } from '../middlewares/AuthValidation';
 
 const router: Router = Router();
 
-router.get('/users', getAllUsers);
-router.get('/users/:id', validateGetUser ,getUserById);
 router.post('/signin', login, loginUser);
 router.post('/signup', register, createAccount);
-router.put('/users/update/:id', authenticateToken, validateUpdateUser, updateUser);
 
 export default router;
